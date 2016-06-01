3c74ebfeeac2a43cc70a04052cb0cfb0b942a5b2
rahman <orisdev@gmail.com>
2016-05-31 22:41:40 -0400

    build successful

--- bpmsuite-monolith/project/bpmsuite-monolith-web/src/main/java/com/rhc/bpm/rest/service/ProcessService.java
+++ bpmsuite-monolith/project/bpmsuite-monolith-web/src/main/java/com/rhc/bpm/rest/service/ProcessService.java
@@ -1,11 +1,17 @@
 package com.rhc.bpm.rest.service;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
 public interface ProcessService {
 
-	
-	
-/**
- * Starts process
- */
-public ResponseEntity<String> startProcess();
+	/**
+	 * Starts process
+	 */
+	@GET
+	@Path("startprocess")
+	@Produces("text/plain")
+	public String startProcess(@QueryParam("processName")String processName);
 }
---
+++ bpmsuite-monolith/project/bpmsuite-monolith-web/src/main/java/com/rhc/bpm/rest/service/impl/ProcessServiceImpl.java
@@ -0,0 +1,14 @@
+package com.rhc.bpm.rest.service.impl;
+
+import com.rhc.bpm.rest.service.ProcessService;
+
+public class ProcessServiceImpl implements ProcessService {
+
+	@Override
+	public String startProcess(String processName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
