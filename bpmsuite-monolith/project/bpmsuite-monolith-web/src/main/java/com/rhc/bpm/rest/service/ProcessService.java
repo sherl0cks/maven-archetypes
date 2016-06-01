867364ba376620bb0c1807e351094f01ba0641cc
rahman <orisdev@gmail.com>
2016-05-31 23:30:29 -0400

    starting restImpl refactor

--- bpmsuite-monolith/project/bpmsuite-monolith-web/src/main/java/com/rhc/bpm/rest/service/ProcessService.java
+++ bpmsuite-monolith/project/bpmsuite-monolith-web/src/main/java/com/rhc/bpm/rest/service/ProcessService.java
@@ -1,17 +1,21 @@
 package com.rhc.bpm.rest.service;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
+import com.rhc.aggregates.Customer;
+
 public interface ProcessService {
+
 
 	/**
 	 * Starts process
 	 */
-	@GET
+	@POST
 	@Path("startprocess")
 	@Produces("text/plain")
-	public String startProcess(@QueryParam("processName")String processName);
+	public Long startProcess( @QueryParam("processId") String processName, @QueryParam("groupId") String groupId,
+			@QueryParam("artifactId") String artificatId,@QueryParam("version")String version, Customer customer);
 }
--- bpmsuite-monolith/project/bpmsuite-monolith-web/src/main/java/com/rhc/bpm/rest/service/impl/ProcessServiceImpl.java
+++ bpmsuite-monolith/project/bpmsuite-monolith-web/src/main/java/com/rhc/bpm/rest/service/impl/ProcessServiceImpl.java
@@ -1,14 +1,24 @@
 package com.rhc.bpm.rest.service.impl;
 
+import org.jbpm.services.api.DeploymentService;
+import org.jbpm.services.api.RuntimeDataService;
+import org.jbpm.services.api.UserTaskService;
+
+import com.rhc.aggregates.Customer;
 import com.rhc.bpm.rest.service.ProcessService;
 
 public class ProcessServiceImpl implements ProcessService {
-
+	private ProcessService processService;
+	private RuntimeDataService runtimeDataService;
+	private DeploymentService deploymentService;
+	private UserTaskService userTaskService;
 	@Override
-	public String startProcess(String processName) {
+	public Long startProcess(String processName, String groupId, String artificatId, String version,
+			Customer customer) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	
 
 }
